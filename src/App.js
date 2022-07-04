b3b710cee06dec2cfc8fa1315091af9c92613795
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2022-07-04 17:50:16 -0300

    Add energy and velocity state controls

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import IconButton from "@mui/material/IconButton";
+import Bolt from "@mui/icons-material/Bolt";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [energy, setEnergy] = React.useState(false);
+  const [velocity, setVelocity] = React.useState("slow");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          data-energy={energy}
+          data-velocity={velocity}
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <IconButton
+          aria-label="Energy"
+          size="large"
+          onClick={() => setEnergy(!energy)}
+          color={energy ? "secondary" : "default"}
         >
-          Learn React
-        </a>
+          <Bolt />
+        </IconButton>
+
+        <ToggleButtonGroup
+          value={velocity}
+          exclusive
+          color="secondary"
+          onChange={(event, newVelocity) => setVelocity(newVelocity)}
+        >
+          <ToggleButton value="slow">Slow</ToggleButton>
+          <ToggleButton value="medium">Medium</ToggleButton>
+          <ToggleButton value="fast">Fast</ToggleButton>
+        </ToggleButtonGroup>
       </header>
     </div>
   );
